0af3ab9563c8d7914270792d771d413311d81694
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-23 12:28:39 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -4,10 +4,10 @@ def main():
     print ("Dame un número:")
     number= int (input())
     
-    if (number==0):
-        print ("es cero")
-    elif (number<0):
+    if (number>0):
         print ("es negativo")
+    elif (number==0):
+        print ("Es cero")
     else:
         print ("es positivo")
     pass
