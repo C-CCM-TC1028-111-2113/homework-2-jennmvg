090ea7b6d4f24e3cbe8f74c14762e43a1f2e4375
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-23 23:24:42 -0500

    Add files via upload

---
+++ assignments/02Licencia/src/licencia.py
@@ -0,0 +1,16 @@
+import math
+print ("Ingresa tu edad")
+edad= int(input())
+
+if (edad>=18):
+    id=print (input(("¿Tienes identificación oficial?(s/n)") )
+        if (id=="s"):
+            print ("Trámite de licencia concedido")
+        elif (id=="n"):
+            print ("No cumples requisios")
+        else
+            print ("Respuesta incorrecta")
+elif (edad<18):
+    print ("No cumples con los requisitos")
+else
+    print ("Respuesta incorrecta")
