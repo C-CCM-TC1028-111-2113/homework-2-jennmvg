31425fe746c93b6d1d46d44e2a77121dd855ac7d
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-24 12:59:33 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,25 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    import math
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: "))
+z = int(input("Ingresa el tercer número: "))
+
+
+if (x>y and y>z):
+    print ("", x,"-", y, "-", z)
+elif (y>x and x>z):
+    print ("", y,"-", x, "-", z)
+elif (z>x and x>y):
+    print ("", z,"-", x, "-", y)
+elif (z>y and y>x):
+    print ("", z,"-", y, "-", x)
+elif (x>z and z>y):
+    print ("", x,"-", z, "-", y)
+elif (y>z and z>x):
+    print ("", y,"-", z, "-", x)
+else:
+    print("Se ingresaron números iguales")
     pass
 
 
