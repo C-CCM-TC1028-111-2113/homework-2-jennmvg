132473623fd15a43801e5d5b64994d8c919cc25d
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-24 13:08:19 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+peso = float(input("Peso en kg: "))
+altura = float(input("Altura en m: "))
+
+IMC= peso/(altura**2)
+
+if (IMC<20):
+    print ("PESO BAJO")
+elif(20<=IMC<25):
+    print ("NORMAL")
+elif (25<=IMC<30):
+    print ("SOBREPESO")
+elif (30<=IMC<40):
+    print ("OBESIDAD")
+elif (IMC>=40):
+    print ("OBESIDAD MORBIDA")
+    
     pass
 if __name__=='__main__':
     main()
