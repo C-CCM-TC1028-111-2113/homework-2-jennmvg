687e269aa76e10b0c658ce4977dbb4bdf1b9152a
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-23 23:24:21 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,22 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    import math
+print ("Ingresa tu edad")
+edad= int(input())
+
+if (edad>=18):
+    id=print (input(("¿Tienes identificación oficial?(s/n)") )
+        if (id=="s"):
+            print ("Trámite de licencia concedido")
+        elif (id=="n"):
+            print ("No cumples requisios")
+        else
+            print ("Respuesta incorrecta")
+elif (edad<18):
+    print ("No cumples con los requisitos")
+else
+    print ("Respuesta incorrecta")
     pass
 
 
