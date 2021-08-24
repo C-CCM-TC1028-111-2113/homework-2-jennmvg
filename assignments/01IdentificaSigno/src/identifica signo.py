aaa89702c267b03884f5b5723993cfb1c94bc3cd
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-08-23 22:43:28 -0500

    Add files via upload

---
+++ assignments/01IdentificaSigno/src/identifica signo.py
@@ -0,0 +1,17 @@
+import math
+def main():
+    #escribe tu código abajo de esta línea
+    print ("Dame un número:")
+    number= int (input())
+    
+    if (number==0):
+        print ("es cero")
+    elif (number<0):
+        print ("es negativo")
+    else:
+        print ("es positivo")
+    pass
+    
+
+if __name__ == '__main__':
+    main()
